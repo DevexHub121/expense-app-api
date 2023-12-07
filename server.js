fe82bae9a3f11f38cbe56dc9410165a7fc39c14b
developmenexpert121 <developmenexpert121@gmail.com>
2023-12-07 12:54:46 +0530

    fixed cors issue

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use(
 );
 
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to Expense application." });
 });
 
 app.use(function (req, res, next) {
